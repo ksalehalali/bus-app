3ec5ac777684a04b7e35336b9ad2d9aa104ced04
abdullahsoubeih <asoubeih@routesme.com>
2022-04-25 00:44:49 +0200

    Some refinements

--- lib/common_src/widget/image_loader.dart
+++ lib/common_src/widget/image_loader.dart
@@ -27,67 +27,7 @@ getAvatarImageWidget(String? imageUrl, Color defaultIconColor, double size) {
           return Icon(Icons.account_circle, color: defaultIconColor, size: size *2);
         },
       ),
-
-      //Image.network('imageUrl', fit: BoxFit.cover),
     ),
-  );
-
- /*
-  return CircleAvatar(
-    radius: 30.0,
-    backgroundImage:
-    NetworkImage('https://via.placeholder.com/150'),
-
-    backgroundColor: Colors.transparent,
-  );
-*/
-  /*
-  return Container(width: size, height: size, decoration: BoxDecoration(border: Border.all(
-    color: Colors.white70,
-  ),
-      borderRadius: BorderRadius.all(Radius.circular(20))
-  ),
-  child: Image.network(
-    '${NetworkConstants().baseUrl}$imageUrl',
-    fit: BoxFit.cover,
-
-    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-      if (loadingProgress == null) return child;
-      return Center(
-        child: CircularProgressIndicator(
-          value: loadingProgress.expectedTotalBytes != null
-              ? loadingProgress.cumulativeBytesLoaded /
-              loadingProgress.expectedTotalBytes!
-              : null,
-        ),
-      );
-    },
-    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-      return Icon(Icons.account_circle, color: defaultIconColor, size: size);
-    },
-  ),
-  );
-  */
-
-
-    Image.network(
-    '${NetworkConstants().baseUrl}$imageUrl',
-    fit: BoxFit.fill,
-
-    loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-      if (loadingProgress == null) return child;
-      return Center(
-        child: CircularProgressIndicator(
-          value: loadingProgress.expectedTotalBytes != null
-              ? loadingProgress.cumulativeBytesLoaded /
-              loadingProgress.expectedTotalBytes!
-              : null,
-        ),
-      );
-    },
-    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-      return Icon(Icons.account_circle, color: defaultIconColor, size: size);
-    },
   );
 
   /*
